69275180da8862bf521547021766c8dba08b7fcd
zsiskos <43017972+zsiskos@users.noreply.github.com>
2020-01-31 13:25:47 -0500

    created game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,22 +1,45 @@
-const cards = ['Queen', 'Queen', 'King', 'King'];
+const cards = [
+	{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png",
+	},
+	{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png",
+	},
+	{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png",
+	},
+	{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds",
+	},
+];
+
 const cardsInPlay = [];
 
 function checkForMatch() {
 	if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You've found a match!");
+		alert("You've found a match!");
 	} else {
-		console.log("Sorry, try again.");
+		alert("Sorry, try again.");
 	}
 	}
-}
+};
 
 function flipCard(cardId) {
-	
-	console.log("User flipped " + cards[cardId])
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 	cardsInPlay.push(cards[cardId]);
 	checkForMatch();
-}
+};
 
 flipCard(0);
 flipCard(2);
