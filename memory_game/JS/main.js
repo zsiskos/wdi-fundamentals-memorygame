b2cdf91d6a3cd545dff7bae0bd72ba3c24ad904a
zsiskos <43017972+zsiskos@users.noreply.github.com>
2020-01-31 11:54:44 -0500

    created game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,20 +1,27 @@
 const cards = ['Queen', 'Queen', 'King', 'King'];
 const cardsInPlay = [];
 
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-
-let cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
-if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You've found a match!");
-} else {
-	alert("Sorry, try again.");
+function checkForMatch() {
+	if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You've found a match!");
+	} else {
+		console.log("Sorry, try again.");
+	}
+	}
 }
+
+function flipCard(cardId) {
+	
+	console.log("User flipped " + cards[cardId])
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
 }
+
+flipCard(0);
+flipCard(2);
+
+
+
 
 
