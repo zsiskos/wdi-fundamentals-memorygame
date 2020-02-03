ba1527c5ae69356b2151b11377c0d0c86845f691
zsiskos <43017972+zsiskos@users.noreply.github.com>
2020-02-03 11:50:15 -0500

    created game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -17,7 +17,7 @@ const cards = [
 	{
 	rank: "king",
 	suit: "diamonds",
-	cardImage: "images/king-of-diamonds",
+	cardImage: "images/king-of-diamonds.png",
 	},
 ];
 
@@ -25,15 +25,17 @@ const cardsInPlay = [];
 
 function checkForMatch() {
 	if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
+	if (cardsInPlay[0].rank === cardsInPlay[1].rank) {
 		alert("You've found a match!");
 	} else {
 		alert("Sorry, try again.");
-	}
-	}
+	};
+	};
 };
 
-function flipCard(cardId) {
+function flipCard() {
+	let cardId = this.getAttribute('data-id');
+	this.setAttribute('src', cards[cardId].cardImage);
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
@@ -41,8 +43,26 @@ function flipCard(cardId) {
 	checkForMatch();
 };
 
-flipCard(0);
-flipCard(2);
+function createBoard() {
+	for (let i = 0; i < cards.length; i++) {
+		let cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i)
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	};	
+};
+
+createBoard();
+
+// added a reset button to reload page after game
+	let reset = document.getElementById('reset');
+	reset.addEventListener('click', resetGame)
+
+function resetGame() {
+	window.location.reload();
+};
+
 
 
 
